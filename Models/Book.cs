37a9d712407917363de88d8c662946e51a710113
Helyon-Pacheco <helyonftp@hotmail.com>
2022-11-17 18:14:32 -0300

    Adicionado Swagger e iniciando a criação das classes modelo

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+namespace BackendTest.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Edition { get; set; }
+        public string Isbn { get; set; }
+        public string Language { get; set; }
+        public int Pages { get; set; }
+
+    }
+}
