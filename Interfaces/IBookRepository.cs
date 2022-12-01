34472d5f9819ebecec92527de383712058c55024
Amanda Luiza <89023662+amandaluizay@users.noreply.github.com>
2022-12-01 16:16:06 -0300

    DDD implementado

--- Interfaces/IBookRepository.cs
+++
@@ -1,9 +0,0 @@
-using BackendTest.Models;
-
-namespace BackendTest.Interfaces
-{
-    public interface IBookRepository : IRepository<Book>
-    {
-        Task<Book> GetBookByName(string name);
-    }
-}
